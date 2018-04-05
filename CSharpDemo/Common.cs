f8c3d711bef2ac82182df3985f8bc59f1639a650
unknown <itsmebajibabu@gmail.com>
2018-04-06 00:13:34 +0530

    MyFirst code change

---
+++ CSharpDemo/Common.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using OpenQA.Selenium;
+using System.Configuration;
+
+namespace CSharpDemo
+{
+    class Common
+    {
+        public static System.Data.DataTable ReadExcel(string fileName)
+        {
+            string fpath=Path.GetExtension(fileName);
+            string conn = string.Empty;
+            System.Data.DataTable dtexcel = new System.Data.DataTable();
+            if (fpath.CompareTo(".xls") == 0)
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+            else
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [Sheet1$]", con); //here we read data from sheet1  
+                    oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+                }
+                catch(Exception ex) { }
+            }
+            return dtexcel;
+        }
+
+        public static bool WriteToExcel(System.Data.DataTable Tbl, string ExcelFilePath = null)
+        {
+            try
+            {
+                if (Tbl == null || Tbl.Columns.Count == 0)
+                    throw new Exception("ExportToExcel: Null or empty input table!\n");
+
+                // load excel, and create a new workbook
+                Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                excelApp.Workbooks.Add();
+
+                // single worksheet
+                Microsoft.Office.Interop.Excel._Worksheet workSheet = excelApp.ActiveSheet;
+
+                // column headings
+                for (int i = 0; i < Tbl.Columns.Count; i++)
+                {
+                    workSheet.Cells[1, (i + 1)] = Tbl.Columns[i].ColumnName;
+                }
+
+                // rows
+                for (int i = 0; i < Tbl.Rows.Count; i++)
+                {
+                    // to do: format datetime values before printing
+                    for (int j = 0; j < Tbl.Columns.Count; j++)
+                    {
+                        workSheet.Cells[(i + 2), (j + 1)] = Tbl.Rows[i][j];
+                    }
+                }
+
+                // check fielpath
+                if (ExcelFilePath != null && ExcelFilePath != "")
+                {
+                    try
+                    {
+                        workSheet.SaveAs(ExcelFilePath);
+                        excelApp.Quit();
+                        return true;
+                        //MessageBox.Show("Excel file saved!");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("ExportToExcel: Excel file could not be saved! Check filepath.\n"
+                            + ex.Message);
+                        return false;
+                    }
+                }
+                else    // no filepath is given
+                {
+                    excelApp.Visible = true;
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ExportToExcel: \n" + ex.Message);
+                return false;
+            }
+        }
+
+        public static void ScreenShot(IWebDriver driver)
+        {
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            string spath = ConfigurationSettings.AppSettings["screenshotPath"].ToString();
+            if (!Directory.Exists(spath))
+            {
+                Directory.CreateDirectory(spath);
+            }
+            string fname= spath + DateTime.Now.ToString("yyyyMMddHHmmss")+".jpeg";
+            ss.SaveAsFile(fname, ScreenshotImageFormat.Jpeg);
+        }
+
+
+    }
+}
---
+++ CSharpDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DataTable dt = Common.ReadExcel(@"C:\Users\Admin\Desktop\test.xlsx");
+                DataTable d1 = new DataTable();
+                d1.Columns.Add("UserId", typeof(string));
+                d1.Columns.Add("Password", typeof(string));
+                d1.Columns.Add("Result", typeof(string));
+
+                foreach (DataRow drr in dt.Rows)
+                {
+                    DataRow dr = d1.NewRow();
+                    dr[0] = drr[1].ToString();
+                    dr[1] = drr[2].ToString();
+                    string baseurl = "http://orangehrm.qedgetech.com/symfony/web/index.php/auth/login";
+                    var driver = new ChromeDriver();
+                    driver.Url = baseurl;
+                    driver.FindElement(By.Id("txtUsername")).SendKeys(drr[1].ToString());
+                    driver.FindElement(By.Id("txtPassword")).SendKeys(drr[2].ToString());
+                    driver.FindElement(By.Id("btnLogin")).Click();
+                    Thread.Sleep(1500);
+
+                    if (driver.Url.Equals("http://orangehrm.qedgetech.com/symfony/web/index.php/dashboard"))
+                    {
+                        Console.WriteLine("{0} | {1} | Passed", drr[1], drr[2]);
+                        dr[2] = "Passed";
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} | {1} | Failed", drr[1], drr[2]);
+                        dr[2] = "Failed";
+                        Common.ScreenShot(driver);
+                    }
+                    Thread.Sleep(1500);
+                    driver.Close();
+                    d1.Rows.Add(dr);
+                }
+                bool x = Common.WriteToExcel(d1, @"C:\Users\Admin\Desktop\testOutput.xlsx");
+                if (x)
+                {
+                    Console.WriteLine("Result Saved");
+                }
+                else
+                {
+                    Console.WriteLine("Result Unable to Save");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
